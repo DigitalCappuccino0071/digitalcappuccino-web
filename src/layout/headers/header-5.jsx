1672fbaf48df867addcd61ec7c0b257db0f77cd6
GaganVashishth <gagan.akrolix@gmail.com>
2023-06-28 17:46:30 +0530

    add document file for seo

--- src/layout/headers/header-5.jsx
+++ src/layout/headers/header-5.jsx
@@ -4,62 +4,13 @@ import useSticky from "../../hooks/use-sticky";
 import Languages from "./component/languages";
 import MobileMenu from "./mobile-menu";
 import NavMenus from "./nav-menus";
-import Head from "next/head";
-import Script from "next/script";
 
 const HeaderFive = () => {
   const { headerSticky } = useSticky();
   return (
     <React.Fragment>
-      <Head>
-        <meta
-          name="google-site-verification"
-          content="ILs5xsVp4CFP9OqfmzWejeNMwRFRwTIwQGfNXl5OTpc"
-        />
-        <title>
-          Digital Marketing & Website Development Company | Digital Cappuccino
-        </title>
-
-        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-        <meta
-          name="description"
-          content="Digital Cappuccino is a digital marketing Company in India. We provides website Development, Social Media Marketing, PPC Marketing, SEO and Email Marketing Services."
-          key="desc"
-        />
-        <meta name="robots" content="index, follow" />
-        <link
-          rel="canonical"
-          href="https://www.digitalcappuccino.com/"
-          key="canonical"
-        />
-        <meta property="og:title" content="Social Title for Cool Page" />
-        <meta
-          property="og:description"
-          content="And a social description for our cool page"
-        />
-        <meta
-          property="og:image"
-          content="https://example.com/images/cool-page.jpg"
-        />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@YOUR_TWITTER_USERNAME" />
-        <meta name="twitter:title" content="TITLE_FOR_YOUR_PAGE" />
-        <meta name="twitter:description" content="DESCRIPTION_FOR_YOUR_PAGE" />
-        <meta name="twitter:image" content="URL_FOR_YOUR_IMAGE" />
-      </Head>
-      <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=AW-11192435865`}
-        strategy="afterInteractive"
-      />
-      <Script id="gtag-init" strategy="afterInteractive">
-        {`
-           window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'AW-11192435865');
-        `}
-      </Script>
+      
+      
       <header className="d-none d-lg-block">
         <div
           id="header-sticky"
