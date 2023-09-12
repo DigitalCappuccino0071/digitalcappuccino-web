62a182b38831f489307b1a54cef3036adfb814b8
Anjali1sh <anjali@ackrolix.com>
2023-09-12 13:21:12 +0530

    seo

--- src/components/seo.jsx
+++ src/components/seo.jsx
@@ -6,16 +6,21 @@ const SEO = ({pageTitle, font}) => (
     <>
         <Head>
             <title>
-                {pageTitle && `${pageTitle} || Digital Cappuccino - We Are Digital Marketing Advisors`}
+                {/* {pageTitle && `${pageTitle} || Digital Cappuccino - We Are Digital Marketing Advisors`} */}
+                Advertising Agency in Delhi - Digital Cappuccino
             </title>
             <meta httpEquiv="x-ua-compatible" content="ie=edge"/>
             <meta name="description"
-                  content="Digital Cappuccino is the digital marketing partner for all businesses looking forward to playing with online media and growing their businesses with successive results."/>
+                  content="Digital Cappuccino is a  Advertising Agency in Delhi agency in Delhi that helps businesses of all sizes grow their online presence. including web design, SEO, PPC."/>
             <meta name="robots" content="index, follow"/>
             <meta
                 name="viewport"
                 content="width=device-width, initial-scale=1, shrink-to-fit=no"
             />
+            <meta property="og:title" content=" Advertising Agency in Delhi - Digital Cappuccino" />
+        <meta
+          property="og:description"
+          content="Digital Cappuccino is a  Advertising Agency in Delhi agency in Delhi that helps businesses of all sizes grow their online presence. including web design, SEO, PPC."/>
             {/* <meta name="google-site-verification" content="U9aKcrxVP2tdcuqCiLv34-jTZaj3CZ7Zew92miNTyIA" /> */}
 
 
