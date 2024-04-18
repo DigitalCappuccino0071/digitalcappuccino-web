6ca839a29fd8ac14f328c9820d26f6c462ff8f72
TusharDC <tushar@digitalcappuccino.com>
2024-04-18 16:16:37 +0530

    meta updated

--- src/components/services/new-services/service-social-media-marketing/index.jsx
+++ src/components/services/new-services/service-social-media-marketing/index.jsx
@@ -1,54 +1,57 @@
-import React, { useEffect } from 'react';
-import { FooterFour, FooterThree, Header, Wrapper } from '../../../../layout';
-import { animationCreate } from '../../../../utils/utils';
-import Breadcrumb from '../../../common/breadcrumb/breadcrumb';
-import CtaTwo from '../../../common/cta/cta-2';
-import CaseArea from '../../../common/recent-case-studies';
-import TestimonialArea from './testimonial-area';
-import ServicesWeb from './services-web';
-import CardDetails from '../../service-1/card-details';
-import CardWeb from './Card-web';
-import AccordionArea from './accordion';
-import ClientFeedback from '../../../homes/home-5/client-feedback';
-import Head from 'next/head';
+import React, { useEffect } from "react";
+import { FooterFour, FooterThree, Header, Wrapper } from "../../../../layout";
+import { animationCreate } from "../../../../utils/utils";
+import Breadcrumb from "../../../common/breadcrumb/breadcrumb";
+import CtaTwo from "../../../common/cta/cta-2";
+import CaseArea from "../../../common/recent-case-studies";
+import TestimonialArea from "./testimonial-area";
+import ServicesWeb from "./services-web";
+import CardDetails from "../../service-1/card-details";
+import CardWeb from "./Card-web";
+import AccordionArea from "./accordion";
+import ClientFeedback from "../../../homes/home-5/client-feedback";
+import Head from "next/head";
 
 const ServiceSocialMediaMarketing = () => {
-  useEffect(() => {
-    setTimeout(() => {
-      animationCreate();
-    }, 500);
-  }, []);
+    useEffect(() => {
+        setTimeout(() => {
+            animationCreate();
+        }, 500);
+    }, []);
 
-  return (
-    <Wrapper>
-      <Head>
-        <title>Social Media Marketing Agency in Gurgaon</title>
-        <meta
-          name="description"
-          content="Digital Cappuccino is the best social media marketing agency in Gurgaon. We offer social media services at the best prices to grow your brand."
-        />
-        <link
-          rel="canonical"
-          href="https://www.digitalcappuccino.com/social-media-marketing-services-gurgaon"
-        />
-        <meta
-          property="og:title"
-          content="Social Media Marketing Agency in Gurgaon"
-        />
-        <meta
-          property="og:description"
-          content="Digital Cappuccino is the best social media marketing agency in Gurgaon. We offer social media services at the best prices to grow your brand."
-        />
-        <meta
-          property="og:url"
-          content="https://www.digitalcappuccino.com/social-media-marketing-services-gurgaon"
-        />
-        <meta
-          property="og:image"
-          content="https://www.digitalcappuccino.com/assets/img/service/3.png"
-        />
-        <script type="application/ld+json">
-          {`
+    return (
+        <Wrapper>
+            <Head>
+                <title>
+                    {" "}
+                    #1 Creative Social Media Marketing Agency in Gurgaon
+                </title>
+                <meta
+                    name="description"
+                    content="Looking for a Social Media Marketing Agency in Gurgaon? Digital Cappuccino is the best social media marketing agency in Gurgaon. We offer professional social media services and marketing programs to reach your goals."
+                />
+                <link
+                    rel="canonical"
+                    href="https://www.digitalcappuccino.com/social-media-marketing-services-gurgaon"
+                />
+                <meta
+                    property="og:title"
+                    content=" #1 Creative Social Media Marketing Agency in Gurgaon"
+                />
+                <meta
+                    property="og:description"
+                    content="Looking for a Social Media Marketing Agency in Gurgaon? Digital Cappuccino is the best social media marketing agency in Gurgaon. We offer professional social media services and marketing programs to reach your goals."
+                />
+                <meta
+                    property="og:url"
+                    content="https://www.digitalcappuccino.com/social-media-marketing-services-gurgaon"
+                />
+                <meta
+                    property="og:image"
+                    content="https://www.digitalcappuccino.com/assets/img/service/3.png"
+                />
+                <script type="application/ld+json">
+                    {`
   {
     "@context": "http://schema.org",
     "@type": "WebPage",
@@ -76,24 +79,26 @@ const ServiceSocialMediaMarketing = () => {
     ]
   }
   `}
-        </script>
-      </Head>
-      <Header />
-      <Breadcrumb
-        color={'#fec4c559'}
-        imagePath={'/assets/img/service/3.png'}
-        title={'Digitalcappuccino: Social Media Marketing Company in Gurgaon'}
-        alt="Socail Media Marketing Company in Gurgaon"
-      />
-      <CardWeb />
-      <ServicesWeb />
-      {/*<TestimonialArea style_2={true}/>*/}
-      <ClientFeedback />
-      <AccordionArea />
-      <CtaTwo />
-      <FooterFour />
-    </Wrapper>
-  );
+                </script>
+            </Head>
+            <Header />
+            <Breadcrumb
+                color={"#fec4c559"}
+                imagePath={"/assets/img/service/3.png"}
+                title={
+                    "Digitalcappuccino: Social Media Marketing Company in Gurgaon"
+                }
+                alt="Socail Media Marketing Company in Gurgaon"
+            />
+            <CardWeb />
+            <ServicesWeb />
+            {/*<TestimonialArea style_2={true}/>*/}
+            <ClientFeedback />
+            <AccordionArea />
+            <CtaTwo />
+            <FooterFour />
+        </Wrapper>
+    );
 };
 
 export default ServiceSocialMediaMarketing;
