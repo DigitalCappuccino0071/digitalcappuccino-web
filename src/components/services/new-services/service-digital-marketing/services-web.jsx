f1639cf50b585111aae2500e661219da92c64dd0
TusharDC <tushar@digitalcappuccino.com>
2025-01-22 13:13:45 +0530

    digital marketing

--- src/components/services/new-services/service-digital-marketing/services-web.jsx
+++ src/components/services/new-services/service-digital-marketing/services-web.jsx
@@ -1,29 +1,35 @@
-import React from 'react';
-import {} from '../../../../data';
-import SingleServiceFour from '../../../common/single-service-4';
-import services_data_digital_marketing from '../../../../data/services-data-digital-marketing';
+import React from "react";
+import {} from "../../../../data";
+import SingleServiceFour from "../../../common/single-service-4";
+import services_data_digital_marketing from "../../../../data/services-data-digital-marketing";
 
 const services_items = services_data_digital_marketing.filter(
-  ser => ser.service_p_4
+    (ser) => ser.service_p_4
 );
 
 const ServicesWeb = () => {
-  return (
-    <div>
-      <div className="tp-service-area pt-50 mb-50">
-        <div className="container">
-          <div className="row gx-17">
-            <div className="project-section-box-two text-center">
-              <h2 className="tp-title-sm">Service We Provide</h2>
+    return (
+        <div>
+            <div className="tp-service-area pt-50 mb-50">
+                <div className="container">
+                    <div className="row gx-17">
+                        <div className="project-section-box-two text-center">
+                            <h2 className="tp-title-sm">Services We Provide</h2>
+                        </div>
+                        {services_items.map((ser, i) => {
+                            return (
+                                <SingleServiceFour
+                                    key={i}
+                                    service={ser}
+                                    index={i}
+                                />
+                            );
+                        })}
+                    </div>
+                </div>
             </div>
-            {services_items.map((ser, i) => {
-              return <SingleServiceFour key={i} service={ser} index={i} />;
-            })}
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default ServicesWeb;
